ac8b70cee7785e457095f413ba47ffe9c542c3bc
Jaafar Mohammed <jafa@Jaafars-MacBook-Pro.local>
2021-02-27 14:59:27 +0300

    deleted some files

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD-h4QJtQnqzq8T6z-0NwfIq7WvHuksCjY",
+    authDomain: "clone-87c8c.firebaseapp.com",
+    projectId: "clone-87c8c",
+    storageBucket: "clone-87c8c.appspot.com",
+    messagingSenderId: "503183106896",
+    appId: "1:503183106896:web:f9ef2260be295b48088255",
+    measurementId: "G-821WJZPC07"
+  };
